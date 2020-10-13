9e925b531cae280c13be85ac39ca09def5b67f20
Joo <wow1548@naver.com>
2020-10-13 13:54:20 +0900

    Init

---
+++ src/main/java/dev/jooz/de/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package dev.jooz.de.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        http.authorizeRequests()
+                .antMatchers("/","/login/**","/css/**","/images/**","/js/**","/console/**")
+                .permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .csrf().disable();
+    }
+}
---
+++ src/test/java/dev/jooz/de/DeApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jooz.de;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
