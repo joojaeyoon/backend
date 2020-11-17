f27dabf8341d9bfd8af84e268d85b19f349d7920
Joo <wow1548@naver.com>
2020-11-17 11:04:16 +0900

    Create Dummy Data

---
+++ src/main/resources/db/migration/V2__dummy.sql
@@ -0,0 +1,23 @@
+INSERT INTO account(email,username,password) VALUES('test@test.com','testuser','testpass');
+
+INSERT INTO post(account_id,category,title,content,price) VALUES(1,'category','test title','test content',35000);
+INSERT INTO post(account_id,category,title,content,price) VALUES(1,'category','test title','test content',35000);
+INSERT INTO post(account_id,category,title,content,price) VALUES(1,'category','test title','test content',35000);
+INSERT INTO post(account_id,category,title,content,price) VALUES(1,'category','test title','test content',35000);
+INSERT INTO post(account_id,category,title,content,price) VALUES(1,'category','test title','test content',35000);
+INSERT INTO post(account_id,category,title,content,price) VALUES(1,'category','test title','test content',35000);
+INSERT INTO post(account_id,category,title,content,price) VALUES(1,'category','test title','test content',35000);
+INSERT INTO post(account_id,category,title,content,price) VALUES(1,'category','test title','test content',35000);
+INSERT INTO post(account_id,category,title,content,price) VALUES(1,'category','test title','test content',35000);
+INSERT INTO post(account_id,category,title,content,price) VALUES(1,'category','test title','test content',35000);
+INSERT INTO post(account_id,category,title,content,price) VALUES(1,'category','test title','test content',35000);
+INSERT INTO post(account_id,category,title,content,price) VALUES(1,'category','test title','test content',35000);
+INSERT INTO post(account_id,category,title,content,price) VALUES(1,'category','test title','test content',35000);
+INSERT INTO post(account_id,category,title,content,price) VALUES(1,'category','test title','test content',35000);
+INSERT INTO post(account_id,category,title,content,price) VALUES(1,'category','test title','test content',35000);
+INSERT INTO post(account_id,category,title,content,price) VALUES(1,'category','test title','test content',35000);
+INSERT INTO post(account_id,category,title,content,price) VALUES(1,'category','test title','test content',35000);
+INSERT INTO post(account_id,category,title,content,price) VALUES(1,'category','test title','test content',35000);
+INSERT INTO post(account_id,category,title,content,price) VALUES(1,'category','test title','test content',35000);
+INSERT INTO post(account_id,category,title,content,price) VALUES(1,'category','test title','test content',35000);
+INSERT INTO post(account_id,category,title,content,price) VALUES(1,'category','test title','test content',35000);
