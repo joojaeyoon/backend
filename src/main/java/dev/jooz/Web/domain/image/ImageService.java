c05210b5d4e21d38301c64f0280e4ede96ab6d42
Joo <wow1548@naver.com>
2020-11-19 15:38:12 +0900

    Separate image check and write file util from ImageService

--- src/main/java/dev/jooz/Web/domain/image/ImageService.java
+++ src/main/java/dev/jooz/Web/domain/image/ImageService.java
@@ -4,6 +4,7 @@ import dev.jooz.Web.domain.image.exception.NoFileUploadException;
 import dev.jooz.Web.domain.image.exception.NoImageException;
 import dev.jooz.Web.domain.image.exception.TooManyImageException;
 import dev.jooz.Web.domain.post.Post;
+import dev.jooz.Web.util.ImageFile;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,47 +12,18 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.UUID;
 
 @Service
 @Transactional
 @RequiredArgsConstructor
 public class ImageService {
     private final ImageRepository imageRepository;
-    private final String[] imgExt= {"png","jpg"};
+    private final ImageFile imageFile;
 
     private final Logger logger= LoggerFactory.getLogger(this.getClass());
 
-    public boolean checkExt(String ext){
-        for(String e : imgExt) {
-            if (ext.equals(e))
-                return true;
-        }
-        return false;
-    }
-
-    public String write(MultipartFile file) throws IOException {
-        UUID uuid=UUID.randomUUID();
-        String name=file.getOriginalFilename();
-        name=uuid.toString()+"-"+name;
-        Path path= Paths.get("src/main/resources/static/images/",name);
-
-        File f=new File(path.toString());
-        f.createNewFile();
-
-        try(OutputStream os= Files.newOutputStream(path)){
-            os.write(file.getBytes());
-        }
-        return name;
-    }
 
     public List<ImageDto.ImageCreateDto> saveImages(MultipartFile[] files) throws Exception{
         List<ImageDto.ImageCreateDto> resDtos=new ArrayList<>();
@@ -69,13 +41,13 @@ public class ImageService {
 
             logger.info("ext = "+ext);
 
-            if(!checkExt(ext)) {
+            if(!imageFile.checkExt(ext)) {
                 throw new NoImageException(ext);
             }
         }
 
         for(int i=0;i<files.length;i++){
-            String name=write(files[i]);
+            String name=imageFile.write(files[i]);
             resDtos.add(new ImageDto.ImageCreateDto(name));
         }
 
@@ -83,7 +55,6 @@ public class ImageService {
     }
 
     public void save(List<ImageDto.ImageCreateDto> dtos, Post post){
-        logger.info(dtos.stream().toArray().toString());
         for(int i=0;i<dtos.size();i++){
             imageRepository.save(dtos.get(i).toEntity(post));
         }
---
+++ src/main/java/dev/jooz/Web/util/ImageFile.java
@@ -0,0 +1,38 @@
+package dev.jooz.Web.util;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.UUID;
+
+public class ImageFile {
+    private final String[] imgExt= {"png","jpg","jpeg"};
+
+    public boolean checkExt(String ext){
+        for(String e : imgExt) {
+            if (ext.equals(e))
+                return true;
+        }
+        return false;
+    }
+
+    public String write(MultipartFile file) throws IOException {
+        UUID uuid=UUID.randomUUID();
+        String name=file.getOriginalFilename();
+        name=uuid.toString()+"-"+name;
+        Path path= Paths.get("src/main/resources/static/images/",name);
+
+        File f=new File(path.toString());
+        f.createNewFile();
+
+        try(OutputStream os= Files.newOutputStream(path)){
+            os.write(file.getBytes());
+        }
+        return name;
+    }
+}
